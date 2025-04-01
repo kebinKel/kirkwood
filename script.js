560517d39c8648cdad18bdcf41c720a5f43ba978
unknown <kebinkelvini@gmail.com>
2025-04-01 07:39:36 +0100

    inital commit

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let bar = document.querySelector('.fa-bars');
-let nav = document.querySelector('nav');
+let close = document.querySelector('.fa-close');
+let nav = document.querySelector('.links');
+let dim = document.querySelector('.dim');
 
 bar.addEventListener('click', ()=> {
-    nav.classList.toggle('open')
+    
+    bar.classList.add('close')
+    close.classList.add('open')
+    nav.classList.add('open')
+    dim.classList.add('open')
+})
+
+close.addEventListener('click', ()=> {
+    
+    bar.classList.remove('close')
+    close.classList.remove('open')
+    nav.classList.remove('open')
+    dim.classList.remove('open')
 })
